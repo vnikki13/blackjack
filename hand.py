fc96d545f6abe42727524e2722e2418d90627d8c
Nikki Vaughan <vnikki13@gmail.com>
2022-09-11 16:09:56 -0700

    Add initial winning logic and UI interaction

--- hand.py
+++ hand.py
@@ -17,3 +17,20 @@ class Hand:
 
         # Use formatting for UI spacing
         print()
+
+    def get_value(self):
+        self.value = 0
+        has_ace = False
+
+        for card in self.cards:
+            self.value += int(card.rank["value"])
+            if (card.rank["rank"] == "A"):
+                has_ace = True
+
+            if has_ace and self.value > 21:
+                self.value -= 10
+
+        return self.value
+
+    def is_blackjack(self):
+        return True if self.get_value() == 21 else False
